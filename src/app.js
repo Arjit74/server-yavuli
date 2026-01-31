68e47519dcc2ed5e65db4270a33e8e67d91f14ab
Kishlaya Mishra <kishlayamishra@gmail.com>
2026-01-31 12:17:07 +0530

    try fixing explore page issue again

--- src/app.js
+++ src/app.js
@@ -22,9 +22,6 @@ app.use(cors({
   },
   credentials: true
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
